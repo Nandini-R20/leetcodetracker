d6250f0cc0abd4bd1473fdf0edaa84d482a0f5b0
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-08-11 12:23:00 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.java
@@ -0,0 +1,11 @@
+// Last updated: 8/11/2026, 12:23:00 PM
+import java.math.BigInteger;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+       BigInteger a=new BigInteger(num1);
+       BigInteger b=new BigInteger(num2);
+       BigInteger sum=a.add(b);
+        return sum.toString();
+    }
+}
