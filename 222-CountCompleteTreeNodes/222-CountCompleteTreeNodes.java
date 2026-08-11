d17f12646995bc52ed0e59f923cc4a495535e63e
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-08-11 12:24:21 +0530

    Create 222-CountCompleteTreeNodes

---
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -0,0 +1,24 @@
+// Last updated: 8/11/2026, 12:24:20 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+        return 1+ countNodes(root.right)+countNodes(root.left);
+         
+        
+    }
+}
