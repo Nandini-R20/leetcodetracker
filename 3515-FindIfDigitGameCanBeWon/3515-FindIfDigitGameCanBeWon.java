4286518c21fb523a537b4ffe3f1c986e9787eb1e
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-08-11 12:20:26 +0530

    Create 3515-FindIfDigitGameCanBeWon

---
+++ 3515-FindIfDigitGameCanBeWon/3515-FindIfDigitGameCanBeWon.java
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 12:20:25 PM
+
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10){
+                sum1+=nums[i];
+            }
+            else{
+                sum2+=nums[i];
+            }
+        }
+        if(sum1 == sum2){
+            return false;
+        }
+        return true;
+    }
+}
+
+
