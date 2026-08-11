2a48522c1842980b47c929cf5de2aa2f61312bf9
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-08-11 12:22:26 +0530

    Create 733-FloodFill

---
+++ 733-FloodFill/733-FloodFill.java
@@ -0,0 +1,19 @@
+// Last updated: 8/11/2026, 12:22:26 PM
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+         int originalColor=image[sr][sc];
+         if(originalColor==color) return image;
+         dfs(image,sr,sc,originalColor,color);
+         return image;
+
+    }
+    public static void dfs(int[][] image,int r,int c,int originalColor,int color){
+        if(r<0|| r>=image.length || c<0|| c>=image[0].length) return;
+        if(image[r][c]!=originalColor) return;
+        image[r][c]=color;
+        dfs(image,r-1,c,originalColor,color);
+        dfs(image,r+1,c,originalColor,color);
+        dfs(image,r,c-1,originalColor,color);
+        dfs(image,r,c+1,originalColor,color);
+    }
+}
