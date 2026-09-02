417f79b5fc6b931f4cfbd441f83714bc46d615f7
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:45:12 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.py
@@ -0,0 +1,21 @@
+# Last updated: 9/2/2026, 12:45:08 PM
+class Solution:
+    def countAndSay(self, n):
+        result = "1"
+
+        for _ in range(n - 1):
+            current = ""
+            i = 0
+
+            while i < len(result):
+                j = i
+
+                while j < len(result) and result[j] == result[i]:
+                    j += 1
+
+                current += str(j - i) + result[i]
+                i = j
+
+            result = current
+
+        return result
