41e0bfd565863dedf2f7ccea6352a72a6d925093
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:41:59 +0530

    Create 127-WordLadder

---
+++ 127-WordLadder/127-WordLadder.py
@@ -0,0 +1,27 @@
+# Last updated: 9/2/2026, 12:41:55 PM
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord, endWord, wordList):
+        words = set(wordList)
+
+        if endWord not in words:
+            return 0
+
+        queue = deque([(beginWord, 1)])
+
+        while queue:
+            word, steps = queue.popleft()
+
+            if word == endWord:
+                return steps
+
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    new_word = word[:i] + c + word[i + 1:]
+
+                    if new_word in words:
+                        words.remove(new_word)
+                        queue.append((new_word, steps + 1))
+
+        return 0
