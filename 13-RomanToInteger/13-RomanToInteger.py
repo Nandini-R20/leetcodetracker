50713645cacd0cca3b4251a9bd2f7314ae0eae06
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:45:59 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,22 @@
+# Last updated: 9/2/2026, 12:45:56 PM
+class Solution:
+    def romanToInt(self, s):
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                result -= values[s[i]]
+            else:
+                result += values[s[i]]
+
+        return result
