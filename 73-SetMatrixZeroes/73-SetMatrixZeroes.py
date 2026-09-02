4de3b709850ef2ecf98ae7a6732366d598498588
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:43:40 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -0,0 +1,16 @@
+# Last updated: 9/2/2026, 12:43:37 PM
+class Solution:
+    def setZeroes(self, matrix):
+        rows = set()
+        cols = set()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
