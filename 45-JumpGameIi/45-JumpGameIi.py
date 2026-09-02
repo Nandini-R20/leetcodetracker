adeca4de1e64be55222b73a7897b9c5eb5933730
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:44:52 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.py
@@ -0,0 +1,15 @@
+# Last updated: 9/2/2026, 12:44:48 PM
+class Solution:
+    def jump(self, nums):
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
