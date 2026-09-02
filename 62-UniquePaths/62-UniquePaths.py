ff7b7d12660738b52627906f1a613258f4466c62
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:44:08 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.py
@@ -0,0 +1,10 @@
+# Last updated: 9/2/2026, 12:44:04 PM
+class Solution:
+    def uniquePaths(self, m, n):
+        dp = [1] * n
+
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+
+        return dp[-1]
