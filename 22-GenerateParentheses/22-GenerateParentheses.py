be3190b0139d7abd0986b4d15a39f30617736125
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:45:42 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -0,0 +1,27 @@
+# Last updated: 9/2/2026, 12:45:38 PM
+class Solution:
+    def generateParenthesis(self, n):
+        result = []
+
+        def backtrack(current, open_count, close_count):
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+
+            if open_count < n:
+                backtrack(
+                    current + "(",
+                    open_count + 1,
+                    close_count
+                )
+
+            if close_count < open_count:
+                backtrack(
+                    current + ")",
+                    open_count,
+                    close_count + 1
+                )
+
+        backtrack("", 0, 0)
+
+        return result
