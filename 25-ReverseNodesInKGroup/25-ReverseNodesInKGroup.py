a309a4d5f7b7d6b7a3adf8fa89287dff7ae7277c
Nandini-R20 <nandiniradhakrishnan2@gmail.com>
2026-09-02 12:45:31 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.py
@@ -0,0 +1,31 @@
+# Last updated: 9/2/2026, 12:45:28 PM
+class Solution:
+    def reverseKGroup(self, head, k):
+        dummy = ListNode(0)
+        dummy.next = head
+
+        group_prev = dummy
+
+        while True:
+            kth = group_prev
+
+            for _ in range(k):
+                kth = kth.next
+
+                if not kth:
+                    return dummy.next
+
+            group_next = kth.next
+
+            prev = group_next
+            current = group_prev.next
+
+            while current != group_next:
+                temp = current.next
+                current.next = prev
+                prev = current
+                current = temp
+
+            temp = group_prev.next
+            group_prev.next = kth
+            group_prev = temp
